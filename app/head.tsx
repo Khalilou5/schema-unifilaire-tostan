8335c9fd967ada5eaf3028aaa275abbf5a13a121
Khalilou5 <khalilouibra@gmail.com>
2025-11-16 21:43:25 +0000

    Create head.tsx

---
+++ app/head.tsx
@@ -0,0 +1,8 @@
+export default function Head() {
+  return (
+    <>
+      <title>Schéma Unifilaire TOSTAN</title>
+      <meta name="description" content="Visualisation interactive du schéma unifilaire TOSTAN" />
+    </>
+  );
+}
